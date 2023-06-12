eb5b93bf47fd3d9b324f739525a64f1cbfa59c7b
ashiqrahmanab <ashiqrahmanab@gmail.com>
2023-06-12 17:50:20 -0400

    Update Email send

--- form-to-email.php
+++ form-to-email.php
@@ -1,10 +1,20 @@
 <?php
-// The message
-$message = "Line 1\r\nLine 2\r\nLine 3";
+$to = "ashiqrahmanab@gmail.com";
+$subject = "Hello from PHP";
+$message = "This is a test email message.";
 
-// In case any of our lines are larger than 70 characters, we should use wordwrap()
-$message = wordwrap($message, 70, "\r\n");
+// Set additional headers
+$headers = "From: sender@example.com\r\n";
+$headers .= "Reply-To: sender@example.com\r\n";
+$headers .= "CC: cc@example.com\r\n";
 
-// Send
-mail('aa10277@nyu.edu', 'My Subject', $message);
+// Send the email
+$mailSent = mail($to, $subject, $message, $headers);
+
+// Check if the email was sent successfully
+if ($mailSent) {
+    echo "Email sent successfully.";
+} else {
+    echo "Failed to send email.";
+}
 ?>
